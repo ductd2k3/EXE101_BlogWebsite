85b650e9a39b2e29bbe17ee5542c5a0f9e14af34
ductd10062003 <ductdhe176150@fpt.edu.vn>
2025-03-25 21:07:07 +0700

    update

---
+++ EXE101/LingoVerse/Pages/User/Community.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LingoVerse.Pages.User
+{
+    public class CommunityModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ EXE101/LingoVerse/Pages/User/CommunityDetail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LingoVerse.Pages.User
+{
+    public class CommunityDetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
